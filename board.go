95310b65504a4ea0b72e5d840f0a4d80d7e148b6
Evan Brown <evanbrown@google.com>
2015-06-24 07:25:04 -0700

    broken

--- board.go
+++ board.go
@@ -9,64 +9,90 @@ import (
 )
 
 type Board struct {
-	cells   [][]int
+	squares [][]*Square
 	slen    int // length of the square
 	padding int // padding when printing
 }
 
+type Square struct {
+	possible []int
+	val      int
+	row      int
+	col      int
+}
+
+func (s *Square) String() string {
+	return fmt.Sprintf("%v,%v", s.row, s.col)
+}
+
+func (b *Board) Peers(s *Square) []*Square {
+	peers := append(b.squaresInRow(s), b.squaresInCol(s)...)
+	peers = append(peers, b.squaresInGroup(s)...)
+	return peers
+}
+
 func (b *Board) Duplicate() *Board {
 	b2 := *b
-	nc := make([][]int, len(b.cells))
+	nc := make([][]*Square, len(b.squares))
 	for i := range nc {
-		nc[i] = make([]int, len(b.cells[i]))
-		copy(nc[i], b.cells[i])
+		nc[i] = make([]*Square, len(b.squares[i]))
+		copy(nc[i], b.squares[i])
 	}
-	b2.cells = nc
+	b2.squares = nc
 	return &b2
 }
 
-func (b *Board) NextEmptyCell(r int, c int) (int, int) {
-	if r == 0 && c == 0 && b.cells[r][c] == 0 {
-		return r, c
+func (b *Board) Flatten() []*Square {
+	squares := make([]*Square, len(b.squares)*len(b.squares))
+	for r := range b.squares {
+		for c := range b.squares[r] {
+			squares[r*len(b.squares)+c] = b.squares[r][c]
+		}
 	}
-	for r, c = b.NextCell(r, c); r != -1 && b.cells[r][c] != 0; {
-		r, c = b.NextCell(r, c)
-	}
-	return r, c
+	return squares
 }
-func (b *Board) NextCell(r int, c int) (int, int) {
+
+func (b *Board) NextEmptySquare() *Square {
+	var s *Square
+	for s = b.squares[0][0]; s.row != -1 && s.val != 0; {
+		s = b.NextSquare(s)
+	}
+	return s
+}
+
+func (b *Board) NextSquare(s *Square) *Square {
 	// -1 if last cell
-	if r+1 == b.slen*b.slen && c+1 == b.slen*b.slen {
-		return -1, -1
-	} else if c+1 < b.slen*b.slen {
-		return r, c + 1
+	if s.row+1 == b.slen*b.slen && s.col+1 == b.slen*b.slen {
+		return NewSquare(0, -1, -1)
+	} else if s.col+1 < b.slen*b.slen {
+		return b.squares[s.row][s.col+1]
 	} else {
-		return r + 1, 0
+		return b.squares[s.row+1][0]
 	}
 }
 
-func (b *Board) Print() string {
+func (b *Board) String() string {
 	var board string
 
 	var v string
-	for r := range b.cells {
+	for r := range b.squares {
 		if r%b.slen == 0 {
 			board += fmt.Sprintf("%v\n", strings.Repeat("-", b.slen*b.slen*3+(b.slen+1)*2-1))
 		}
-		for c := range b.cells[r] {
+		for c := range b.squares[r] {
 			if c%b.slen == 0 {
 				board += fmt.Sprintf("| ")
 			}
-			if b.cells[r][c] == 0 {
+			if b.squares[r][c].val == 0 {
 				v = "*"
 			} else {
-				v = strconv.Itoa(b.cells[r][c])
+				v = strconv.Itoa(b.squares[r][c].val)
 			}
 			board += fmt.Sprintf("%2s ", v)
 		}
 		board += fmt.Sprintf("|\n")
 	}
-	board += fmt.Sprintf("%v", strings.Repeat("-", b.slen*b.slen*3+(b.slen+1)*2-1))
+	board += fmt.Sprintf("%v\n", strings.Repeat("-", b.slen*b.slen*3+(b.slen+1)*2-1))
 	return board
 }
 
@@ -77,13 +103,11 @@ func (b *Board) Shuffle(a []int) {
 	}
 }
 
-func (b *Board) AvailableVals(r int, c int) []int {
-	used := append(b.valsInRow(r), b.valsInCol(c)...)
-	used = append(used, b.valsInSquare(r, c)...)
+func (b *Board) availableVals(s *Square) []int {
 	p := b.possibleVals()
-
-	for i := range used {
-		delete(p, used[i])
+	peers := b.Peers(s)
+	for i := range peers {
+		delete(p, peers[i].val)
 	}
 	available := make([]int, 0, len(p))
 	for k := range p {
@@ -103,49 +127,49 @@ func (b *Board) possibleVals() map[int]bool {
 	return p
 }
 
-func (b *Board) valsInRow(r int) []int {
-	vals := make([]int, b.slen*b.slen)
+func (b *Board) squaresInRow(s *Square) []*Square {
+	vals := make([]*Square, b.slen*b.slen)
 	for i := 0; i < len(vals); i++ {
-		vals[i] = b.cells[r][i]
+		vals[i] = b.squares[s.row][i]
 	}
 	return vals
 }
 
-func (b *Board) valsInCol(c int) []int {
-	vals := make([]int, b.slen*b.slen)
+func (b *Board) squaresInCol(s *Square) []*Square {
+	vals := make([]*Square, b.slen*b.slen)
 	for i := 0; i < len(vals); i++ {
-		vals[i] = b.cells[i][c]
+		vals[i] = b.squares[i][s.col]
 	}
 	return vals
 }
 
-func (b *Board) valsInSquare(r int, c int) []int {
-	vals := make([]int, b.slen*b.slen*b.slen)
+func (b *Board) squaresInGroup(s *Square) []*Square {
+	squares := make([]*Square, b.slen*b.slen)
 
-	rr, cc := b.TopLeftCoord(r, c)
+	firstSquare := b.firstSquareInGroup(s)
 	i := 0
-	for r = rr; r < rr+b.slen; r++ {
-		for c = cc; c < cc+b.slen; c++ {
-			vals[i] = b.cells[r][c]
+	for r := firstSquare.row; r < firstSquare.row+b.slen; r++ {
+		for c := firstSquare.col; c < firstSquare.col+b.slen; c++ {
+			squares[i] = b.squares[r][c]
 			i++
 		}
 	}
-	return vals
+	return squares
 }
 
-func (b *Board) TopLeftCoord(r int, c int) (int, int) {
+func (b *Board) firstSquareInGroup(s *Square) *Square {
 	var rr, cc int
-	if r%b.slen == 0 {
-		rr = r
+	if s.row%b.slen == 0 {
+		rr = s.row
 	} else {
-		rr = r - r%b.slen
+		rr = s.row - s.row%b.slen
 	}
-	if c%b.slen == 0 {
-		cc = c
+	if s.col%b.slen == 0 {
+		cc = s.col
 	} else {
-		cc = c - c%b.slen
+		cc = s.col - s.col%b.slen
 	}
-	return rr, cc
+	return b.squares[rr][cc]
 }
 
 func BoardFromS(s string) *Board {
@@ -156,7 +180,7 @@ func BoardFromS(s string) *Board {
 		if c == '.' {
 			c = '0'
 		}
-		board.cells[i/9][i%9] = int(c - '0')
+		board.squares[i/9][i%9] = NewSquare(int(c-'0'), i/9, i%9)
 	}
 	return board
 }
@@ -173,9 +197,14 @@ func NewBoard(slen int) *Board {
 	}
 	b.slen = slen
 	b.padding = 5
-	b.cells = make([][]int, b.slen*b.slen)
-	for i := range b.cells {
-		b.cells[i] = make([]int, b.slen*b.slen)
+	b.squares = make([][]*Square, b.slen*b.slen)
+	for r := range b.squares {
+		b.squares[r] = make([]*Square, b.slen*b.slen)
 	}
 	return b
+}
+
+func NewSquare(val int, row int, col int) *Square {
+	s := &Square{val: val, row: row, col: col}
+	return s
 }
--- sudoku.go
+++ sudoku.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"runtime"
 )
@@ -8,40 +9,60 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	v1 := HardBoard()
-	done := make(chan *Board)
-	defer close(done)
-	r, c := v1.NextEmptyCell(0, 0)
-	go Solve(v1, done, r, c)
-	go Solve(v1.Duplicate(), done, r, c)
-	go Solve(v1.Duplicate(), done, r, c)
-	go Solve(v1.Duplicate(), done, r, c)
+	PropagateAvailable(v1, v1.Flatten())
+	Solve(v1)
 	fmt.Println("Waiting for solution...")
-	solution := <-done
-	fmt.Println(solution.Print())
+	fmt.Println(v1)
 }
 
-func Solve(b *Board, done chan *Board, r int, c int) bool {
-	// Get the available values for this cell
-	//fmt.Printf("\033[3;1H")
-	//time.Sleep(0 * time.Millisecond)
-	a := b.AvailableVals(r, c)
-	for v := range a {
-		b.cells[r][c] = a[v]
-		//fmt.Printf(b.Print())
-		// Get next cell
-		if nr, nc := b.NextEmptyCell(r, c); nr != -1 {
-			// If an error is returned, try the next value by continuing in the loop
-			if err := Solve(b, done, nr, nc); err == true {
-				continue
-			} else {
-				return false
+// Propagate available values to provided squares
+func PropagateAvailable(b *Board, squares []*Square) error {
+	for i := range squares {
+		s := squares[i]
+		s.possible = b.availableVals(s)
+
+		if len(s.possible) == 0 {
+			return errors.New(fmt.Sprintf("Error: No solutions for %v", s))
+		}
+
+		if len(s.possible) == 1 && s.val == 0 {
+			s.val = s.possible[0]
+			fmt.Printf("\033[3;1H")
+			fmt.Println(b)
+			if err := PropagateAvailable(b, b.Peers(s)); err != nil {
+				s.val = 0
+				PropagateAvailable(b, b.Peers(s))
 			}
+		}
+	}
+	return nil
+}
+
+func Solve(b *Board) bool {
+	s := b.NextEmptySquare()
+	if s.row == -1 {
+		fmt.Println("Finished!!")
+		return false
+	}
+	fmt.Printf("\033[3;1H")
+	for v := 0; v < len(s.possible); v++ {
+		s.val = s.possible[v]
+		fmt.Println(b)
+		if err := PropagateAvailable(b, b.Peers(s)); err != nil {
+			s.val = 0
+			PropagateAvailable(b, b.Peers(s))
+			return true
+		}
+		// Propagation may have solved the puzzle. Check
+		if b.NextEmptySquare().row == -1 {
+			return false
+		}
+		// If an error is returned, try the next value by continuing in the loop
+		if err := Solve(b); err == true {
+			continue
 		} else {
-			// No more cells. We're done
-			done <- b
 			return false
 		}
 	}
-	b.cells[r][c] = 0
-	return true
+	return false
 }
